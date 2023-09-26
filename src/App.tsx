f98bcb95ec95a5648ba32f68cf79428e06b1b167
Weel <430784@student.fontys.nl>
2023-09-26 15:43:10 +0200

    updated connection string

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="app">
       Hello World!
-      <VideoFeed src="http://localhost:8083/stream/27aec28e-6181-4753-9acd-0456a75f0289/channel/0/hls/live/index.m3u8" />
+      <VideoFeed src="https://rtsp.mweel.nl/stream/27aec28e-6181-4753-9acd-0456a75f0289/channel/0/hls/live/index.m3u8" />
     </div>
   );
 }
